692810fc166fbfb85929d6a8c159f7e2f0ce7b5f
Waf888 <113286025+Waf888@users.noreply.github.com>
2025-09-22 08:38:50 +0200

    Update MainPage.xaml.cs

--- MauiApp2/MainPage.xaml.cs
+++ MauiApp2/MainPage.xaml.cs
@@ -1,16 +1,46 @@
-﻿using System.Collections.ObjectModel;
+using Microsoft.Maui.Controls;
+using System.Collections.ObjectModel;
+
 namespace MauiApp2
 {
     public partial class MainPage : ContentPage
     {
+        ObservableCollection<Item> Products { get; set; }
 
-
-        public ObservableCollection<Item> Products { get; set; };
         public MainPage()
         {
             InitializeComponent();
             Products = new ObservableCollection<Item>();
             BindingContext = this;
+        }
+
+        private void AddButton_Clicked(object sender, EventArgs e)
+        {
+            string nazwa = AddItem.Text;
+            if (string.IsNullOrEmpty(nazwa))
+                return;
+            AddItem.Text = "";
+
+            var JuzDodany = Products.FirstOrDefault(p => p.Name.Equals(nazwa, StringComparison.OrdinalIgnoreCase));
+
+            if (JuzDodany != null)
+            {
+                JuzDodany.Quantity++;
+                Products.Remove(JuzDodany);
+                Products.Add(JuzDodany);
+            }
+            else
+            {
+                Products.Add(new Item { Name = nazwa, Quantity = 1 });
+            }
+
+            wyswietl.ItemsSource = Products;
+        }
+
+        private void DelButton_Clicked(object sender, EventArgs e)
+        {
+            var selected = wyswietl.SelectedItem;
+            Products.Remove((Item)selected);
         }
     }
 }
